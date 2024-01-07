181fa531feb19441768e49de7bf3635de450082b
Samanth612 <rocketspeed35@gmail.com>
2024-01-07 20:27:31 +0530

    Vectors

---
+++ Arrays/vector.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    vector<int> v(5);
+    for (auto &x:v){
+        cin >> x;
+    }
+    for (auto &x:v){
+        cout << x<<" ";
+    }
+    cout << "\n" << v[0]<<" => Element at index 0\n";
+    cout << v.size()<<" => Size of vector\n";
+    cout << v.at(0)<<"\n";
+    sort(v.begin(),v.end());
+    for (auto &x:v){
+        cout << x<<" ";
+    }
+    return 0;
+}
