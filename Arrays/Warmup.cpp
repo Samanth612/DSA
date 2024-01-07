04983272cdb1caf084a64e1082ee87c9426ead97
Samanth612 <rocketspeed35@gmail.com>
2024-01-07 21:49:50 +0530

    Arrays

--- Arrays/Warmup.cpp
+++ Arrays/Warmup.cpp
@@ -3,15 +3,19 @@
 using namespace std;
 
 int main(){
-    int arr[5];
+
+    int n;
+    cin >> n;
+
+    int arr[n];
 
     //Reading array elements
-    for (int i = 0; i < 5;i++){
+    for (int i = 0; i < n;i++){
         cin >> arr[i];
     }
     
     //Printing array elements
-    for (int i = 0; i < 5;i++){
+    for (int i = 0; i < n;i++){
         cout << arr[i]<<" ";
     }
     return 0;
