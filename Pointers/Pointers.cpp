bd06fedeaa79c43066207ef196fe72095f23e11a
Samanth612 <rocketspeed35@gmail.com>
2024-01-13 12:15:59 +0530

    Pointers

--- Pointers/Pointers.cpp
+++ Pointers/Pointers.cpp
@@ -14,7 +14,17 @@ int main(){
 
     cout << p<<"\n"; // This gives address of a
 
-    cout << *p; // This gives the value which is present inside a
+    cout << *p << endl; // This gives the value which is present inside a
+
+    int *arr;
+
+    arr = (int *)malloc(5 * sizeof(int)); // This is the syntax to access heap memory in c
+
+    cout << arr<<endl;
+
+    arr = new int[5]; // This the syntax to access heap memory in c++
+
+    cout << arr;
 
     return 0;
 }
