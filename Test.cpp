1f1f5e204993ec9ad5a6d1f80180a6b0e28423b7
Samanth612 <rocketspeed35@gmail.com>
2024-01-13 13:16:54 +0530

    Added test file to clear doubts

--- Test.cpp
+++ Test.cpp
@@ -10,7 +10,9 @@ struct Test{
 int main()
 {
     struct Test *p,r;
+
     cout << sizeof(p) << " => p \n"; // Always takes 4 bytes it is based on platform
+    
     cout<< sizeof(r) << " => r";
 
     return 0;
