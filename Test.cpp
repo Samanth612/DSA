d3ea2a59cbf3ebfee024011d89e8b3a3e55eab3b
Samanth612 <rocketspeed35@gmail.com>
2024-01-14 12:21:33 +0530

    Added Functions

--- Test.cpp
+++ Test.cpp
@@ -11,9 +11,17 @@ int main()
 {
     struct Test *p,r;
 
-    cout << sizeof(p) << " => p \n"; // Always takes 4 bytes it is based on platform
+    cout << sizeof(p) << " => p \n"; // Always takes 4 bytes it is based on platform, oniline gdb takes 8 bytes others 4 bytes
     
     cout<< sizeof(r) << " => r";
+
+    int a = 10;
+
+    int pointer;
+
+    int *ptr;
+
+    ptr = &a; //(pointer = &a;) inside bracket gives error as => a value of type "int *" cannot be assigned to an entity of type "int"
 
     return 0;
 }
