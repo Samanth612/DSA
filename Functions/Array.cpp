3abf097fbd9e4a60fe2dfd26623128361c9bc836
Samanth612 <rocketspeed35@gmail.com>
2024-01-14 13:26:22 +0530

    Array as Parameter to function

--- Functions/Array.cpp
+++ Functions/Array.cpp
@@ -8,10 +8,37 @@ int * fun(int n){
     return p;
 }
 
+int * fun1(int n){
+    int *p;
+    p = new int[n];
+    for (int i = 0; i < n;i++){
+        p[i] = i + 1;
+    }
+    return p;
+}
+
+void fun2(int *arr,int n){
+    for (int i = 0; i < n;i++){
+        cout << arr[i] << " ";
+    }
+}
+
 int main(){
 
     int *p;
     p = fun(5);
-    cout << p;
+    cout << p<<endl;
+    p = fun1(5);
+    for (int i = 0; i < 5;i++)
+    {
+        cout << p[i]<<" ";
+    }
+    
+    cout << endl;
+
+    int arr[] = {10, 20, 30, 40, 50};
+
+    fun2(arr,5);
+
     return 0;
 }
